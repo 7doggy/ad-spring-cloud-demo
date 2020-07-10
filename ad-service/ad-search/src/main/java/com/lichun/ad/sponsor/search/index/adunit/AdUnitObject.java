2bde2eadb16ab75c2809acdf9db0c757cd4911d8
7doggy <seven.doggy@gmail.com>
2020-07-10 22:00:37 +0800

    增加单元索引

---
+++ ad-service/ad-search/src/main/java/com/lichun/ad/sponsor/search/index/adunit/AdUnitObject.java
@@ -0,0 +1,37 @@
+package com.lichun.ad.sponsor.search.index.adunit;
+
+import com.lichun.ad.sponsor.search.index.adplan.AdPlanObject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdUnitObject {
+
+    private Long unitId;
+    private Integer unitStatus;
+    private Integer positionType;
+    private Long planId;
+
+    private AdPlanObject adPlanObject;
+
+    void update(AdUnitObject newObject) {
+        if (null != newObject.unitId) {
+            this.unitId = newObject.getUnitId();
+        }
+        if (null != newObject.getUnitStatus()) {
+            this.unitStatus = newObject.getUnitStatus();
+        }
+        if (null != newObject.getPositionType()) {
+            this.positionType = newObject.getPositionType();
+        }
+        if (null != newObject.getPlanId()) {
+            this.planId = newObject.getPlanId();
+        }
+        if (null != newObject.getAdPlanObject()) {
+            this.adPlanObject = newObject.getAdPlanObject();
+        }
+    }
+}
